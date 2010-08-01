723f68f0598260374360a74838e00917a32ad3dc
Bernd Stramm <bernd.stramm@gmail.com>
2010-08-01 12:22:57 -0400

    script useful for packaging

---
+++ pack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NAME=egalite
+VERSION=0.1.0
+PACKAGEDIR=${HOME}/packaging/dchat
+
+makearchive.sh ${NAME}-${VERSION} master
+cp ${NAME}-${VERSION}.tar.gz ${PACKAGEDIR}
+ls -l ${PACKAGEDIR}/${NAME}-${VERSION}.tar.gz
