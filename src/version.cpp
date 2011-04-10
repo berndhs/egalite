a783023b87f2ccb63d8fb8d1f86566e93602ab06
Bernd Stramm <bernd.stramm@gmail.com>
2011-04-10 19:41:11 -0400

    start new version

--- src/version.cpp
+++ src/version.cpp
@@ -29,7 +29,7 @@
 
 namespace deliberate {
 
-  QString ProgramVersion::VersionNumber ("0.5.0");
+  QString ProgramVersion::VersionNumber ("0.5.1");
 
   QString ProgramVersion::ProgramName("Egalite");
   QString ProgramVersion::copyright ("Copyright (C) 2010 Bernd H Stramm");
